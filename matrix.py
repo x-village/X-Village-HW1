ab905d3b3b61d4d1e187fb6776c541d073a82524
johnnylord <johnnyalan7410@gmail.com>
2018-07-12 22:26:22 +0800

    first commit

---
+++ matrix.py
@@ -0,0 +1,30 @@
+import random
+
+from copy import deepcopy
+
+
+class Matrix:
+
+    def __init__(self, nrows, ncols):
+        """Construct a (nrows X ncols) matrix"""
+        pass
+
+    def add(self, m):
+        """return a new Matrix object after summation"""
+        pass
+
+    def sub(self, m):
+        """return a new Matrix object after substraction"""
+        pass
+
+    def mul(self, m):
+        """return a new Matrix object after multiplication"""
+        pass
+
+    def transpose(self):
+        """return a new Matrix object after transpose"""
+        pass
+    
+    def display(self):
+        """Display the content in the matrix"""
+        pass
